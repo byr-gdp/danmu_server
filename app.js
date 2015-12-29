0d209d5aa8bf7ac58e1eb4810e3f473199945600
byr-gdp <anthozoan77@gmail.com>
2015-12-29 13:27:28 +0800

    update format using jscs

--- app.js
+++ app.js
@@ -1,64 +1,69 @@
+// var cors       = require('cors');
 var express    = require('express');
 var bodyParser = require('body-parser');
-// var cors       = require('cors');
 
-var app = express();
 // app.use(cors);
 // app.options('/get', cors());
 // app.options('/post', cors());
+var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true,
 }));
 
 var items = [];
 
 items.push({
-  msg: "hello",
-  timestamp: Math.round(new Date().getTime()/1000)
+  msg: 'hello',
+  timestamp: Math.round(new Date().getTime() / 1000),
+  mode: 1,
 });
 
 items.push({
-  msg: "This is a test.",
-  timestamp: Math.round(new Date().getTime()/1000)
+  msg: 'This is a test.',
+  timestamp: Math.round(new Date().getTime() / 1000),
+  mode: 1,
 });
 
 items.push({
-  msg: "Don't be afraid.",
-  timestamp: Math.round(new Date().getTime()/1000)
+  msg: 'Don\'t be afraid.',
+  timestamp: Math.round(new Date().getTime() / 1000),
+  mode: 1,
 });
 
 items.push({
-  msg: "Show me your code.",
-  timestamp: Math.round(new Date().getTime()/1000)
+  msg: 'Show me your code.',
+  timestamp: Math.round(new Date().getTime() / 1000),
+  mode: 1,
 });
 
 items.push({
-  msg: "hahaha.",
-  timestamp: Math.round(new Date().getTime()/1000)
+  msg: 'hahaha.',
+  timestamp: Math.round(new Date().getTime() / 1000),
+  mode: 1,
 });
 
-app.get("/get", function(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,HEAD, OPTIONS");
+app.get('/get', function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD, OPTIONS');
   res.send(items);
 });
 
-app.post("/post", function(req, res) {
+app.post('/post', function(req, res) {
   var msg       = req.body.msg;
-  var timestamp = Math.round(new Date().getTime()/1000);
+  var timestamp = Math.round(new Date().getTime() / 1000);
 
   items.shift();
   items.push({
     msg: msg,
-    timestamp: timestamp
+    timestamp: timestamp,
   });
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,HEAD, OPTIONS");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,HEAD, OPTIONS');
   res.send(items);
 });
 
 app.listen(2333, function() {
-  console.log("listen at port 2333");
+  console.log('listen at port 2333');
 });
